c27e4ed6ea3dfb1e5d0d94145e84b6d208e28381
adrianwijaya584 <adrianwijaya584@gmail.com>
2022-04-09 23:52:07 +0800

    change facingMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import QrReader from 'react-qr-scanner'
+import { useState } from 'react';
 
 function App() {
+  const [openCamera, setOpenCamera]= useState(false)
+
   function scan(data) {
     console.log(data);
   }
 
   return (
     <div className="App">
-      <QrReader
-        onScan={scan}
-        style={{
-          height: 240,
-          width: 320,
-        }}
-      />
+      <button onClick={()=>setOpenCamera(v=> !v)}>{openCamera?"close":"open"} Camera</button>
+
+      {
+        openCamera&&<QrReader
+          onScan={scan}
+          facingMode="rear"
+          style={{
+            height: 240,
+            width: 320,
+          }}
+        />
+      }
     </div>
   );
 }
